a9d72eb8fa19d0a1aade142a670e38d8efa16a23
adenilson1 <adenilson99@hotmail.com>
2025-11-17 09:40:20 -0300

    Set comparando igualdade

---
+++ s_02_07_set_hashcode_equals/src/application/Program01.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import entities.Product;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Set<Product> set = new HashSet<Product>();
+
+		set.add(new Product("TV", 900.00));
+		set.add(new Product("Notebook", 1200.00));
+		set.add(new Product("Tablet", 400.00));
+
+		Product prod = new Product("Notebook", 1200.00);
+
+		System.out.println(set.contains(prod));
+
+	}
+
+}
